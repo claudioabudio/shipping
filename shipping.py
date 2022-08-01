569d97a37ab3b4ee5ba0e82466200a7c8fc73e83
Claudio <clau@fakeemail.com>
2022-07-31 23:36:56 -0500

    implemented a staticmethod and a classmethod

--- shipping.py
+++ shipping.py
@@ -1,9 +1,18 @@
 class ShippingContainer:
     next_serial = 1337
 
+    @staticmethod
+    def _generate_serial():
+        result = ShippingContainer.next_serial
+        ShippingContainer.next_serial += 1
+        return result
+
+    @classmethod
+    def create_empty(cls, owner_code):
+        return cls(owner_code, contents=[])
+
     def __init__(self, owner_code, contents):
         self.contents = contents
         self.owner_code = owner_code
-        self.serial = ShippingContainer.next_serial
-        ShippingContainer.next_serial += 1
+        self.serial = ShippingContainer._generate_serial()
     
