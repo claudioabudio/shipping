58f2209b82e42e4c5b3ab5a3a2b3c9b622c1b7aa
Claudio <clau@fakeemail.com>
2022-08-01 19:03:54 -0500

    implements BIC code

--- shipping.py
+++ shipping.py
@@ -1,3 +1,5 @@
+import iso6346
+
 class ShippingContainer:
     next_serial = 1337
 
@@ -7,6 +9,11 @@ class ShippingContainer:
         ShippingContainer.next_serial += 1
         return result
 
+    @staticmethod
+    def _make_bic_code(owner_code, serial):
+        return iso6346.create(owner_code=owner_code,
+            serial=str(serial).zfill(6))
+
     @classmethod
     def create_empty(cls, owner_code):
         return cls(owner_code, contents=[])
@@ -14,5 +21,7 @@ class ShippingContainer:
     def __init__(self, owner_code, contents):
         self.contents = contents
         self.owner_code = owner_code
-        self.serial = ShippingContainer._generate_serial()
+        self.bic = ShippingContainer._make_bic_code(
+            owner_code,
+            ShippingContainer._generate_serial())
     
