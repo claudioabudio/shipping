60a28f119fee1eedd6f6da5b8f5921a34a4b82d3
Claudio <clau@fakeemail.com>
2022-08-20 10:04:42 -0500

    accept kwargs in super class so as to pass extra parameters to the subclasses

--- shipping.py
+++ shipping.py
@@ -1,5 +1,6 @@
 import iso6346
 
+
 class ShippingContainer:
     next_serial = 1337
 
@@ -12,22 +13,35 @@ class ShippingContainer:
     @staticmethod
     def _make_bic_code(owner_code, serial):
         return iso6346.create(owner_code=owner_code,
-            serial=str(serial).zfill(6))
+                              serial=str(serial).zfill(6))
 
     @classmethod
     def create_empty(cls, owner_code):
         return cls(owner_code, contents=[])
 
-    def __init__(self, owner_code, contents):
+    @classmethod
+    def create_with_items(cls, owner_code, items, **kwargs):
+        return cls(owner_code, contents=list(items), **kwargs)
+
+    def __init__(self, owner_code, contents, **kwargs):
         self.contents = contents
         self.owner_code = owner_code
         self.bic = self._make_bic_code(
             owner_code,
             ShippingContainer._generate_serial())
 
+
 class RefrigeratedShippingContainer(ShippingContainer):
+    MAX_CELSIUS = 4.0
+
+    def __init__(self, owner_code, contents, celsius):
+        if celsius > self.MAX_CELSIUS:
+            raise ValueError("Temperature too hot !")
+        super().__init__(owner_code, contents)
+        self.celsius = celsius
+
     @staticmethod
     def _make_bic_code(owner_code, serial):
         return iso6346.create(owner_code=owner_code,
-            serial=str(serial).zfill(6),
-            category='R')
+                              serial=str(serial).zfill(6),
+                              category='R')
