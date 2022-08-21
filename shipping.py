0497f80332d2708678cf80c591acf020025d3995
Claudio <clau@fakeemail.com>
2022-08-21 11:15:49 -0500

    added Position class and implemented __repr__ method

--- shipping.py
+++ shipping.py
@@ -4,6 +4,9 @@ import iso6346
 class ShippingContainer:
     next_serial = 1337
 
+    HEIGHT_FT = 8.5
+    WIDTH_FT = 8.0
+
     @staticmethod
     def _generate_serial():
         result = ShippingContainer.next_serial
@@ -16,26 +19,32 @@ class ShippingContainer:
                               serial=str(serial).zfill(6))
 
     @classmethod
-    def create_empty(cls, owner_code):
-        return cls(owner_code, contents=[])
+    def create_empty(cls, owner_code, length_ft):
+        return cls(owner_code, length_ft, contents=[])
 
     @classmethod
-    def create_with_items(cls, owner_code, items, **kwargs):
-        return cls(owner_code, contents=list(items), **kwargs)
+    def create_with_items(cls, owner_code, length_ft, items, **kwargs):
+        return cls(owner_code, length_ft, contents=list(items), **kwargs)
 
-    def __init__(self, owner_code, contents, **kwargs):
+    def __init__(self, owner_code, length_ft, contents, **kwargs):
         self.contents = contents
         self.owner_code = owner_code
+        self.length_ft = length_ft
         self.bic = self._make_bic_code(
             owner_code,
             ShippingContainer._generate_serial())
 
+    @property
+    def volume_ft3(self):
+        return ShippingContainer.HEIGHT_FT * ShippingContainer.WIDTH_FT * self.length_ft
+
 
 class RefrigeratedShippingContainer(ShippingContainer):
     MAX_CELSIUS = 4.0
+    FRIDGE_VOLUME_FT3 = 100
 
-    def __init__(self, owner_code, contents, celsius):
-        super().__init__(owner_code, contents)
+    def __init__(self, owner_code, length_ft, contents, celsius):
+        super().__init__(owner_code, length_ft, contents)
         self.celsius = celsius
 
     @property
@@ -48,8 +57,17 @@ class RefrigeratedShippingContainer(ShippingContainer):
             raise ValueError("Temperature too hot !")
         self._celsius = value
 
+    @property
+    def volume_ft3(self):
+        return super().volume_ft3 - RefrigeratedShippingContainer.FRIDGE_VOLUME_FT3
+
     @staticmethod
     def _make_bic_code(owner_code, serial):
         return iso6346.create(owner_code=owner_code,
                               serial=str(serial).zfill(6),
                               category='R')
+
+
+class HeatedRefrigeratedShippingContainer(RefrigeratedShippingContainer):
+    MIN_CELSIUS = -20
+
